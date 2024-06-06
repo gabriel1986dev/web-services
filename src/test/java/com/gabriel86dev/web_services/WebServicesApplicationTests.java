07877077be42b63bc678155ccc09a24e8955b44e
gabriel1986dev <etevaldo1986@yahoo.com>
2024-06-06 15:15:54 -0300

    Project created

---
+++ src/test/java/com/gabriel86dev/web_services/WebServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gabriel86dev.web_services;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebServicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
