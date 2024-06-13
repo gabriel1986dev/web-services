49cdc3d84782fb0e2ba46b1c4aec968d75cddb65
gabriel1986dev <etevaldo1986@yahoo.com>
2024-06-13 16:04:24 -0300

    Exception handling - update

--- src/main/java/com/gabriel86dev/web_services/services/UserService.java
+++ src/main/java/com/gabriel86dev/web_services/services/UserService.java
@@ -13,6 +13,8 @@ import com.gabriel86dev.web_services.repositories.UserRepository;
 import com.gabriel86dev.web_services.services.exceptions.DatabaseException;
 import com.gabriel86dev.web_services.services.exceptions.ResourceNotFoundException;
 
+import jakarta.persistence.EntityNotFoundException;
+
 @Service
 public class UserService {
 	
@@ -43,10 +45,13 @@ public class UserService {
 	}
 	
 	public User update(Long id, User obj) {
+		try {
 		User entity = repository.getReferenceById(id);
 		updateData(entity, obj);
 		return repository.save(entity);
-		
+		} catch (EntityNotFoundException e) {
+			throw new ResourceNotFoundException(id);
+		}
 	}
 
 	private void updateData(User entity, User obj) {
